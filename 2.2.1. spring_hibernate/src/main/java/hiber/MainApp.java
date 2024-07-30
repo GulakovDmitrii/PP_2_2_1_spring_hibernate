460988b746a00d51d2504a2ca7fd9bdaefe4534e
GulakovDmitrii <gulakov127@gmail.com>
2024-07-30 19:57:39 +0300

    V1 without getUserByCar

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,24 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("Igor", "Evlanov", "rvlanov@bk.ru");
+      Car bmw = new Car("BMW", 5);
+      user1.setCar(bmw);
+
+      userService.add(user1);
+      User user2 = new User("Vasya", "Pupkin", "pupkin@bk.ru");
+      Car audi = new Car("Audi", 4);
+      user2.setCar(audi);
+      userService.add(user2);
+
+//      user1 = userService.getUserByCar("BMW", 5);
+//      System.out.println(user1.getCar().getModel());
+
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +41,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
 
